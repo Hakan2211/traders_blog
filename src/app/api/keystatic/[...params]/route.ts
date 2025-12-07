5862c46d339c34fe292f1100bd29437487ff6750
hakan <hbilgic1992@gmail.com>
2025-12-07 13:50:53 +0100

    route.ts hardcoded env test

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -3,4 +3,8 @@ import config from '../../../../../keystatic.config';
 
 export const { GET, POST } = makeRouteHandler({
   config,
+  // ⚠️ TEMPORARY: Hardcode keys to test connection
+  clientId: 'Ov23lixV2afUdVhSMnnJ',
+  clientSecret: 'cf6ad7176bfb9ef5293425182abce9937bacd324',
+  secret: 'killuasoldykassasingonfreaksfriend', // Your random string
 });
