47bf8c9de289b2510be71bfed4f9f46f8b8554bf
hakan <hbilgic1992@gmail.com>
2025-12-07 13:27:08 +0100

    add debug route to control env variables form vercel

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -8,8 +8,11 @@ export default config({
       : {
           kind: 'github',
           repo: {
-            owner: 'Hakan2211',
-            name: 'traders_blog',
+            owner:
+              process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER ||
+              'YOUR_GITHUB_USERNAME',
+            name:
+              process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG || 'YOUR_REPO_NAME',
           },
         },
   collections: {
